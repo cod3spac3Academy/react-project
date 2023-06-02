342b30e8fec89461a0ebdbb61d7c5d2cbf33f7f9
ClaraMars <clara.marquez.torres@gmail.com>
2023-06-02 12:35:56 +0200

    No transitions

--- src/components/Slideshow/Slideshow.tsx
+++ src/components/Slideshow/Slideshow.tsx
@@ -9,10 +9,19 @@ import Slide3 from "../../assets/slider_03.jpg";
 const Slideshow = (): JSX.Element => {
 
     const images = [Slide1, Slide2, Slide3];
-    const description = ["CONVENTIONAL FOOD", "OUTSTANDING DINNER", "WONDERFUL CUISINE"];
+    const description = ["CONVENTIONAL FOOD", "OUTSTANDING DINING", "WONDERFUL CUISINE"];
     const title = ["MORNING MOVEMENT", "DELICIOUS COOKING", "AUTHENTIC KITCHEN"];
 
     const [currentIndex, setCurrentIndex] = useState(0);
+    const [imageLoad, setImageLoad] = useState(false);
+
+    const handleOnLoad = () => {
+        setImageLoad(true);
+    }
+
+    // const handleOnLoad = (e: React.SyntheticEvent<HTMLImageElement, Event>) => {
+    //     (e.target as HTMLImageElement).style.transform="translateX(10%)";
+    // }
 
     const handleClickLeft = () => {
         setCurrentIndex(currentIndex - 1);
@@ -25,19 +34,22 @@ const Slideshow = (): JSX.Element => {
         if (currentIndex === 2) {
             setCurrentIndex(0);
         }
+        setImageLoad(true);
     }
 
     useEffect(() => {
         const interval = setInterval(() => {
             handleClickRight();
+            // handleOnLoad(handleClickRight());
         }, 5000);
-        return () => clearInterval(interval);
+        return () => clearInterval(interval); //  Función de desmontaje: cuando se desmonta el componente, se limpia el intervalo para que no haya fugas de memoria.
     }, [currentIndex]);
 
     return (
         <>
             <div className={classes.slideshow}>
-                <img className={classes.image} src={images[currentIndex]} alt={description[currentIndex]} />
+                {/* <img className={classes.image} src={images[currentIndex]} alt={description[currentIndex]} onLoad={(e) => {(e.target as HTMLImageElement).style.transform="translateX(30%)"}}/> */}
+                {imageLoad && <img className={imageLoad ? classes.image : classes.loaded} src={images[currentIndex]} alt={description[currentIndex]} onLoad={handleOnLoad}/>}
                 <div className={classes.slider}>
                     <FontAwesomeIcon icon={faArrowLeftLong} className={classes.arrow} onClick={handleClickLeft}/>
                     <div className={classes.sliderInfo}>
