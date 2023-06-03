f0d895c6cfeeb8d7769ffe629ff9f9cc8baef09b
ClaraMars <clara.marquez.torres@gmail.com>
2023-06-03 16:08:39 +0200

    Slider no dinámico

--- src/components/Slideshow/Slideshow.tsx
+++ src/components/Slideshow/Slideshow.tsx
@@ -8,12 +8,12 @@ import Slide3 from "../../assets/slider_03.jpg";
 
 const Slideshow = (): JSX.Element => {
 
-    const images = [Slide1, Slide2, Slide3];
-    const description = ["CONVENTIONAL FOOD", "OUTSTANDING DINING", "WONDERFUL CUISINE"];
-    const title = ["MORNING MOVEMENT", "DELICIOUS COOKING", "AUTHENTIC KITCHEN"];
+    // const images = [Slide1, Slide2, Slide3];
+    // const description = ["CONVENTIONAL FOOD", "OUTSTANDING DINING", "WONDERFUL CUISINE"];
+    // const title = ["MORNING MOVEMENT", "DELICIOUS COOKING", "AUTHENTIC KITCHEN"];
 
     const [currentIndex, setCurrentIndex] = useState(0);
-    const [currentImage, setCurrentImage] = useState(false);
+    // const [currentImage, setCurrentImage] = useState(false);
 
     const handleClickLeft = () => {
         setCurrentIndex(currentIndex - 1);
@@ -32,10 +32,10 @@ const Slideshow = (): JSX.Element => {
     useEffect(() => {
         const interval = setInterval(() => {
             handleClickRight();
-            setCurrentImage(true);
-            setTimeout(() => {
-                setCurrentImage(false);
-            }, 4000);
+            // setCurrentImage(true);
+            // setTimeout(() => {
+            //     setCurrentImage(false);
+            // }, 4000);
         }, 5000);
         
         return () => {
@@ -45,29 +45,57 @@ const Slideshow = (): JSX.Element => {
 
     return (
         <>
-            <div className={classes.slideshow}>
-                {/* {currentImage ? <img className={classes.loadedBack} src={images[currentIndex]} alt={description[currentIndex]}/> : <img className={classes.loaded} src={images[currentIndex]} alt={description[currentIndex]}/>} */}
-                {/* {currentImage ? <img className={classes.image} src={images[currentIndex]} alt={description[currentIndex]}/> : <img className={classes.imageBack} src={images[currentIndex]} alt={description[currentIndex]}/>} */}
-                {/* {currentImage ? <div className={classes.image}>
+            <div className={classes.slidershow}>
+                {/* <div className={classes.sliderForward}>
                     <img className={classes.image} src={images[currentIndex]} alt={description[currentIndex]}/>
-                </div> : null} */}
-                {/* { */}
-
-                {/* // currentImage && ( */}
-                {/* <div className={classes.sliderForward}> */}
-                <img className={classes.image} src={images[currentIndex]} alt={description[currentIndex]}/>
-                <div className={classes.slider}>
-                    <FontAwesomeIcon icon={faArrowLeftLong} className={classes.arrow} onClick={handleClickLeft}/>
-                    <div className={classes.sliderInfo}>
-                        <h4>{description[currentIndex]}</h4>
-                        <h2>{title[currentIndex]}</h2>
-                        <button>DISCOVER MORE</button>
+                    <div className={classes.slider}>
+                        <FontAwesomeIcon icon={faArrowLeftLong} className={classes.arrow} onClick={handleClickLeft}/>
+                        <div className={classes.sliderInfo}>
+                            <h4>{description[currentIndex]}</h4>
+                            <h2>{title[currentIndex]}</h2>
+                            <button>DISCOVER MORE</button>
+                        </div>
+                        <FontAwesomeIcon icon={faArrowRightLong} className={classes.arrow} onClick={handleClickRight}/>
                     </div>
-                    <FontAwesomeIcon icon={faArrowRightLong} className={classes.arrow} onClick={handleClickRight}/>
-                </div>
-                {/* </div> */}
-                {/* // )
-                // } */}
+                </div> */}
+                
+                {currentIndex === 0 ? (<div className={classes.sliderContent}>
+                    <img className={classes.image} src={Slide1} alt="Conventional Food"/>
+                    <div className={classes.slider}>
+                        <FontAwesomeIcon icon={faArrowLeftLong} className={classes.arrow} onClick={handleClickLeft}/>
+                        <div className={classes.sliderInfo}>
+                            <h4>CONVENTIONAL FOOD</h4>
+                            <h2>MORNING MOVEMENT</h2>
+                            <button>DISCOVER MORE</button>
+                        </div>
+                        <FontAwesomeIcon icon={faArrowRightLong} className={classes.arrow} onClick={handleClickRight}/>
+                    </div>
+                </div>) : (currentIndex === 1 ? (
+                <div className={classes.sliderContent}>
+                    <img className={classes.image} src={Slide2} alt="Outstanding dining"/>
+                    <div className={classes.slider}>
+                        <FontAwesomeIcon icon={faArrowLeftLong} className={classes.arrow} onClick={handleClickLeft}/>
+                        <div className={classes.sliderInfo}>
+                            <h4>OUTSTANDING DINING</h4>
+                            <h2>DELICIOUS COOKING</h2>
+                            <button>DISCOVER MORE</button>
+                        </div>
+                        <FontAwesomeIcon icon={faArrowRightLong} className={classes.arrow} onClick={handleClickRight}/>
+                    </div>
+                </div>) : (
+                <div className={classes.sliderContent}>
+                    <img className={classes.image} src={Slide3} alt="Wonderful cuisine"/>
+                    <div className={classes.slider}>
+                        <FontAwesomeIcon icon={faArrowLeftLong} className={classes.arrow} onClick={handleClickLeft}/>
+                        <div className={classes.sliderInfo}>
+                            <h4>WONDERFUL CUISINE</h4>
+                            <h2>AUTHENTIC KITCHEN</h2>
+                            <button>DISCOVER MORE</button>
+                        </div>
+                        <FontAwesomeIcon icon={faArrowRightLong} className={classes.arrow} onClick={handleClickRight}/>
+                    </div>
+                </div>))}
+                
             </div>
         </>
     )
