fa6e841e304efcdac5ddc1d23528e3144b1cbbda
daniel <danielcgonze@gmail.com>
2023-06-04 11:51:33 +0200

    starting tests

---
+++ src/components/Footer.test.js
@@ -0,0 +1,10 @@
+import Footer from "./Footer";
+import { render } from "@testing-library/react";
+
+describe("Footer", () => {
+  it("should render correctly", () => {
+    const { container } = render(<Footer />);
+  });
+
+
+});
